daa28cba7033753e966e9c4c7e2909ba44ee6b35
mandarinnni <mandarinnni@gmail.com>
2026-02-16 17:13:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,19 @@ ADMIN_ID = 295824298
 user_messages = {}
 admin_reply_mode = {}
 
+from flask import Flask
+import threading
+import os
+
+app_web = Flask(__name__)
+
+@app_web.route("/")
+def home():
+    return "Bot is running"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app_web.run(host="0.0.0.0", port=port)
 
 
 # --- Старт и приветствие ---
