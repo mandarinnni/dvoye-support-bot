11b6010961d35881616b82a5f70c2e5b8479f073
mandarinnni <mandarinnni@gmail.com>
2026-02-17 13:49:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,19 +11,6 @@ ADMIN_ID = 295824298
 user_messages = {}
 admin_reply_mode = {}
 
-from flask import Flask
-import threading
-import os
-
-app_web = Flask(__name__)
-
-@app_web.route("/")
-def home():
-    return "Bot is running"
-
-def run_web():
-    port = int(os.environ.get("PORT", 10000))
-    app_web.run(host="0.0.0.0", port=port)
 
 
 # --- Старт и приветствие ---
@@ -352,13 +339,16 @@ def main():
     app.add_handler(CallbackQueryHandler(buttons))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_messages))
 
-    print("Бот запущен")
+  PORT = int(os.environ.get("PORT", 10000))
+    RENDER_EXTERNAL_URL = os.environ.get("RENDER_EXTERNAL_URL")
 
-    threading.Thread(target=run_web).start()
+    print("Бот запущен через webhook")
 
-    app.run_polling()
-
-
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        webhook_url=f"{RENDER_EXTERNAL_URL}/{TOKEN}"
+    )
 
 if __name__ == "__main__":
     main()
